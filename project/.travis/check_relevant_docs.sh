34b3f4012b334140d6fb7002914b61a40e58b342
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-05-31 16:46:26 +0200

    CS on Travis shell scripts

--- project/.travis/check_relevant_docs.sh
+++ project/.travis/check_relevant_docs.sh
@@ -3,4 +3,4 @@ set -ev
 
 RELEVANT_FILES=$(git diff --name-only HEAD upstream/${TRAVIS_BRANCH} -- '*.rst')
 
-if [[ -z  ${RELEVANT_FILES} ]];then echo -n 'KO'; exit 0; fi;
+if [[ -z ${RELEVANT_FILES} ]]; then echo -n 'KO'; exit 0; fi;
