b4557ea8d21fd04b151eaa612dde05f0e962debe
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-02-18 15:32:17 +0100

    Travis deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-set :deploy_to, '/var/www/sullivansenechal.com/sonata-dev-kit/production'
+set :deploy_to, '/var/www/dev-kit'
 
 SSHKit.config.command_map[:composer] = "php #{shared_path.join("composer.phar")}"
