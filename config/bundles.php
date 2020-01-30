838a481565a4874a153beb70255119daa05c0390
Oskar Stark <oskarstark@googlemail.com>
2020-01-30 10:15:29 +0100

    no bundles

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,4 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
