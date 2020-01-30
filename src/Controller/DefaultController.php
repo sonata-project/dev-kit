d320d5d5a7be9b2bb525461c7a6058a92f1bd135
Oskar Stark <oskarstark@googlemail.com>
2020-01-30 10:30:41 +0100

    Update Controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,6 +16,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 final class DefaultController extends AbstractController
 {
