a3c883e81e0489bc26e3c6ee4194e15cc8a78561
Oskar Stark <oskarstark@googlemail.com>
2020-08-28 07:20:29 +0200

    Fix

--- src/Command/DispatchCommand.php
+++ src/Command/DispatchCommand.php
@@ -187,7 +187,7 @@ final class DispatchCommand extends AbstractNeedApplyCommand
             'Name',
             'Actual color',
             'Needed Color',
-            'State'
+            'State',
         ];
 
         $rows = [];
