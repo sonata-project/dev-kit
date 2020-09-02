15ad090de9a496f4e03ad2a001ea1f66ccd5ec9b
Oskar Stark <oskarstark@googlemail.com>
2020-09-02 11:12:17 +0200

    fix

--- src/Domain/Value/Variant.php
+++ src/Domain/Value/Variant.php
@@ -53,7 +53,7 @@ final class Variant
         return sprintf(
             '%s:"%s"',
             $this->package,
-            'dev-master' === $this->version ? $this->version : ($$this->version.'.*')
+            'dev-master' === $this->version ? $this->version : ($this->version.'.*')
         );
     }
 }
