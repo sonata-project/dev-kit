ac67a207c4c17d7689e7f910b7f233cda8bd92e1
Oskar Stark <oskarstark@googlemail.com>
2020-09-03 09:03:24 +0200

    fix

---
+++ src/Config/Projects.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Config;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Oskar Stark <oskarstark@googlemail.com>
+ */
+final class Projects
+{
+    public function __construct()
+    {
+
+    }
+}
