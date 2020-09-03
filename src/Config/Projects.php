9e74e236b16aed66ad7a0887dba8a5ce28cb412f
Oskar Stark <oskarstark@googlemail.com>
2020-09-03 09:07:54 +0200

    fix

--- src/Config/Projects.php
+++ src/Config/Projects.php
@@ -13,16 +13,34 @@ declare(strict_types=1);
 
 namespace App\Config;
 
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
+use App\Domain\Value\Project;
+use Packagist\Api\Client;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * @author Oskar Stark <oskarstark@googlemail.com>
  */
 final class Projects
 {
-    public function __construct()
-    {
+    private Client $packagist;
 
+    /**
+     * @var array<string, Project>
+     */
+    private array $projects;
+
+    public function __construct(Client $packagist)
+    {
+        $this->packagist = $packagist;
+
+        $processor = new Processor();
+        $devKitConfigs = $processor->processConfiguration(new DevKitConfiguration(), [
+            'sonata' => Yaml::parse(file_get_contents(__DIR__.'/../../config/dev-kit.yml')),
+        ]);
+        $projectsConfigs = $processor->processConfiguration(new ProjectsConfiguration($devKitConfigs), [
+            'sonata' => ['projects' => Yaml::parse(file_get_contents(__DIR__.'/../../config/projects.yml'))],
+        ]);
+        $this->configs = array_merge($devKitConfigs, $projectsConfigs);
     }
 }
