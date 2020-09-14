c6f3fc02da96733dad2f0815277ef4bc77433dc7
Oskar Stark <oskarstark@googlemail.com>
2020-09-14 11:14:43 +0200

    Enhancement: Use Action value object

---
+++ tests/Github/Domain/Value/Webhook/ActionTest.php
@@ -0,0 +1,143 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Github\Domain\Value\Webhook;
+
+use App\Github\Domain\Value\Webhook\Action;
+use PHPUnit\Framework\TestCase;
+
+final class ActionTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function throwsExceptionIfValueIsEmptyString(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Action::fromString('');
+    }
+
+    /**
+     * @test
+     */
+    public function fromString(): void
+    {
+        $value = 'foo';
+
+        self::assertSame(
+            $value,
+            Action::fromString($value)->toString()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function SYNCHRONIZE(): void
+    {
+        self::assertSame(
+            'synchronize',
+            Action::SYNCHRONIZE()->toString()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function CEATED(): void
+    {
+        self::assertSame(
+            'created',
+            Action::CREATED()->toString()
+        );
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider equalsProvider
+     */
+    public function equals(bool $expected, Action $action, Action $other): void
+    {
+        self::assertSame(
+            $expected,
+            $action->equals($other)
+        );
+    }
+
+    public function equalsProvider(): \Generator
+    {
+        yield [
+            true,
+            Action::fromString('synchronize'),
+            Action::SYNCHRONIZE()
+        ];
+
+        yield [
+            false,
+            Action::CREATED(),
+            Action::SYNCHRONIZE()
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider equalsOneOfProvider
+     */
+    public function equalsOneOf(bool $expected, Action $action, array $others): void
+    {
+        self::assertSame(
+            $expected,
+            $action->equalsOneOf($others)
+        );
+    }
+
+    public function equalsOneOfProvider(): \Generator
+    {
+        yield [
+            true,
+            Action::fromString('synchronize'),
+            [
+                Action::SYNCHRONIZE(),
+            ]
+        ];
+
+        yield [
+            true,
+            Action::fromString('synchronize'),
+            [
+                Action::SYNCHRONIZE(),
+                Action::CREATED(),
+            ],
+        ];
+
+        yield [
+            false,
+            Action::CREATED(),
+            [
+                Action::SYNCHRONIZE(),
+            ],
+        ];
+
+        yield [
+            false,
+            Action::CREATED(),
+            [
+                Action::SYNCHRONIZE(),
+                Action::fromString('synchronize'),
+            ],
+        ];
+    }
+}
