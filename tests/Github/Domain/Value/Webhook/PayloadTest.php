d6fc0f9c6977b4cf09e46ab746906383cd79970f
Oskar Stark <oskarstark@googlemail.com>
2020-09-14 11:14:43 +0200

    Enhancement: Use Action value object

--- tests/Github/Domain/Value/Webhook/PayloadTest.php
+++ tests/Github/Domain/Value/Webhook/PayloadTest.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Github\Domain\Value\Webhook;
 
+use App\Github\Domain\Value\Webhook\Action;
 use App\Github\Domain\Value\Webhook\Event;
 use App\Github\Domain\Value\Webhook\Payload;
 use PHPUnit\Framework\TestCase;
@@ -48,7 +49,7 @@ final class PayloadTest extends TestCase
 
         $payload = Payload::fromJsonString($json, $event);
 
-        self::assertSame($action, $payload->action());
+        self::assertSame($action, $payload->action()->toString());
         self::assertSame($issueId, $payload->issueId());
         self::assertSame($issueAuthorId, $payload->issueAuthorId());
         self::assertTrue($payload->isTheCommentFromTheAuthor());
@@ -84,7 +85,7 @@ final class PayloadTest extends TestCase
 
         $payload = Payload::fromJsonString($json, $event);
 
-        self::assertSame($action, $payload->action());
+        self::assertSame($action, $payload->action()->toString());
         self::assertSame($issueId, $payload->issueId());
         self::assertSame($issueAuthorId, $payload->issueAuthorId());
         self::assertTrue($payload->isTheCommentFromTheAuthor());
@@ -120,7 +121,7 @@ final class PayloadTest extends TestCase
 
         $payload = Payload::fromJsonString($json, $event);
 
-        self::assertSame($action, $payload->action());
+        self::assertSame($action, $payload->action()->toString());
         self::assertSame($issueId, $payload->issueId());
         self::assertSame($issueAuthorId, $payload->issueAuthorId());
         self::assertFalse($payload->isTheCommentFromTheAuthor());
@@ -151,7 +152,7 @@ final class PayloadTest extends TestCase
 
         $payload = Payload::fromJsonString($json, $event);
 
-        self::assertSame($action, $payload->action());
+        self::assertSame($action, $payload->action()->toString());
         self::assertSame($issueId, $payload->issueId());
         self::assertSame($issueAuthorId, $payload->issueAuthorId());
         self::assertFalse($payload->isTheCommentFromTheAuthor());
