142a71ecb121bbe987ba979786837486771ecbdd
Sullivan SENECHAL <soullivaneuh@gmail.com>
2020-09-15 10:25:13 +0200

    Applied fixes from FlintCI

--- src/Command/AbstractCommand.php
+++ src/Command/AbstractCommand.php
@@ -20,7 +20,6 @@ use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use function Symfony\Component\String\u;
 use Symfony\Component\Yaml\Yaml;
 
