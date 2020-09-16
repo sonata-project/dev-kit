fab4f21f2973305f23483b99ec01c7d30120fe8c
Oskar Stark <oskarstark@googlemail.com>
2020-09-16 09:28:36 +0200

    Fix: Remove updateLabels() method from DispatchCommand

--- src/Command/DispatchCommand.php
+++ src/Command/DispatchCommand.php
@@ -111,7 +111,6 @@ final class DispatchCommand extends AbstractNeedApplyCommand
                 $this->updateRepositories($package, $projectConfig);
                 $this->deleteHooks($package);
                 $this->updateDevKitHook($package);
-                $this->updateLabels($package);
                 $this->updateBranchesProtection($package, $projectConfig);
 
                 if ($input->getOption('with-files')) {
@@ -168,88 +167,6 @@ final class DispatchCommand extends AbstractNeedApplyCommand
             }
         } else {
             $this->io->comment(static::LABEL_NOTHING_CHANGED);
-        }
-    }
-
-    private function updateLabels(Package $package): void
-    {
-        $repositoryName = Util::getRepositoryNameWithoutVendorPrefix($package);
-        $this->io->section('Labels');
-
-        $configuredLabels = $this->configs['labels'];
-        $missingLabels = $configuredLabels;
-
-        $headers = [
-            'Name',
-            'Actual color',
-            'Needed color',
-            'State',
-        ];
-
-        $rows = [];
-
-        foreach ($this->github->repo()->labels()->all(static::GITHUB_GROUP, $repositoryName) as $label) {
-            $name = $label['name'];
-            $color = $label['color'];
-
-            $shouldExist = \array_key_exists($name, $configuredLabels);
-            $configuredColor = $shouldExist ? $configuredLabels[$name]['color'] : null;
-            $shouldBeUpdated = $shouldExist && $color !== $configuredColor;
-
-            if ($shouldExist) {
-                unset($missingLabels[$name]);
-            }
-
-            $state = null;
-            if (!$shouldExist) {
-                $state = 'Deleted';
-                if ($this->apply) {
-                    $this->github->repo()->labels()->remove(static::GITHUB_GROUP, $repositoryName, $name);
-                }
-            } elseif ($shouldBeUpdated) {
-                $state = 'Updated';
-                if ($this->apply) {
-                    $this->github->repo()->labels()->update(static::GITHUB_GROUP, $repositoryName, $name, [
-                        'name' => $name,
-                        'color' => $configuredColor,
-                    ]);
-                }
-            }
-
-            if ($state) {
-                array_push($rows, [
-                    $name,
-                    '#'.$color,
-                    $configuredColor ? '#'.$configuredColor : 'N/A',
-                    $state,
-                ]);
-            }
-        }
-
-        foreach ($missingLabels as $name => $label) {
-            $color = $label['color'];
-
-            if ($this->apply) {
-                $this->github->repo()->labels()->create(static::GITHUB_GROUP, $repositoryName, [
-                    'name' => $name,
-                    'color' => $color,
-                ]);
-            }
-            array_push($rows, [$name, 'N/A', '#'.$color, 'Created']);
-        }
-
-        usort($rows, static function ($row1, $row2): int {
-            return strcasecmp($row1[0], $row2[0]);
-        });
-
-        if (empty($rows)) {
-            $this->io->comment(static::LABEL_NOTHING_CHANGED);
-        } else {
-            $this->io->table($headers, $rows);
-
-            if ($this->apply) {
-                $this->io->success('Labels successfully updated.');
-            }
         }
     }
 
