64f6fd1ef8724a15eb05a75a86aee27c40ea71b4
Oskar Stark <oskarstark@googlemail.com>
2020-09-16 10:20:03 +0200

    Enhancement: Extract DispatchBranchesProtectionCommand

--- src/Command/DispatchCommand.php
+++ src/Command/DispatchCommand.php
@@ -99,7 +99,6 @@ final class DispatchCommand extends AbstractNeedApplyCommand
                 $package = $this->packagist->get(static::PACKAGIST_GROUP.'/'.$name);
                 $projectConfig = $this->configs['projects'][$name];
                 $this->io->title($package->getName());
-                $this->updateBranchesProtection($package, $projectConfig);
 
                 if ($input->getOption('with-files')) {
                     $this->dispatchFiles($package);
@@ -113,87 +112,6 @@ final class DispatchCommand extends AbstractNeedApplyCommand
         }
 
         return 0;
-    }
-
-    private function updateBranchesProtection(Package $package, array $projectConfig): void
-    {
-        $repositoryName = Util::getRepositoryNameWithoutVendorPrefix($package);
-        $branches = array_keys($projectConfig['branches']);
-        $this->io->section('Branches protection');
-
-        foreach ($branches as $branch) {
-            $requiredStatusChecks = $this->buildRequiredStatusChecks(
-                $branch,
-                $projectConfig['branches'][$branch],
-                $projectConfig['docs_target']
-            );
-
-            if ($this->apply) {
-                $this->github->repo()->protection()
-                    ->update(static::GITHUB_GROUP, $repositoryName, $branch, [
-                        'required_status_checks' => [
-                            'strict' => false,
-                            'contexts' => $requiredStatusChecks,
-                        ],
-                        'required_pull_request_reviews' => [
-                            'dismissal_restrictions' => [
-                                'users' => [],
-                                'teams' => [],
-                            ],
-                            'dismiss_stale_reviews' => true,
-                            'require_code_owner_reviews' => true,
-                        ],
-                        'restrictions' => null,
-                        'enforce_admins' => false,
-                    ]);
-            }
-        }
-
-        if ($this->apply) {
-            $this->io->comment('Branches protection applied.');
-        } else {
-            $this->io->comment(static::LABEL_NOTHING_CHANGED);
-        }
-    }
-
-    private function buildRequiredStatusChecks(string $branchName, array $branchConfig, bool $docsTarget): array
-    {
-        $targetPhp = $branchConfig['target_php'] ?? end($branchConfig['php']);
-        $requiredStatusChecks = [
-            'composer-normalize',
-            'YAML files',
-            'XML files',
-            'PHP-CS-Fixer',
-            sprintf('PHP %s + lowest + normal', reset($branchConfig['php'])),
-        ];
-
-        if ($docsTarget) {
-            $requiredStatusChecks[] = 'Sphinx build';
-            $requiredStatusChecks[] = 'DOCtor-RST';
-        }
-
-        foreach ($branchConfig['php'] as $phpVersion) {
-            $requiredStatusChecks[] = sprintf('PHP %s + highest + normal', $phpVersion);
-        }
-
-        foreach ($branchConfig['variants'] as $variant => $versions) {
-            foreach ($versions as $version) {
-                $requiredStatusChecks[] = sprintf(
-                    'PHP %s + highest + %s:"%s"',
-                    $targetPhp,
-                    $variant,
-                    'dev-master' === $version ? $version : ($version.'.*'),
-                );
-            }
-        }
-
-        $this->io->writeln(sprintf(
-            'Required Status-Checks for <info>%s</info>:',
-            $branchName
-        ));
-        $this->io->listing($requiredStatusChecks);
-
-        return $requiredStatusChecks;
     }
 
     private function dispatchFiles(Package $package): void
