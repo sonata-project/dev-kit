97f386bb2aac9dbec1e3c4e4ed1619375d6477f8
Oskar Stark <oskarstark@googlemail.com>
2020-09-20 14:09:36 +0200

    Enhancement: Add and use dedicated repositories API

---
+++ src/Github/Api/Repositories.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Github\Api;
+
+use App\Domain\Value\Repository;
+use Github\Client as GithubClient;
+use Webmozart\Assert\Assert;
+
+/**
+ * @author Oskar Stark <oskarstark@googlemail.com>
+ */
+final class Repositories
+{
+    private GithubClient $github;
+
+    public function __construct(GithubClient $github)
+    {
+        $this->github = $github;
+    }
+
+    public function show(Repository $repository): array
+    {
+        return $this->github->repo()->show(
+            $repository->vendor(),
+            $repository->name()
+        );
+    }
+
+    public function update(Repository $repository, array $params): void
+    {
+        $this->github->repo()->update(
+            $repository->vendor(),
+            $repository->name(),
+            array_merge(
+                $params,
+                [
+                    'name' => $repository->name(),
+                ]
+            )
+        );
+    }
+
+    public function merge(Repository $repository, string $base, string $head): ?array
+    {
+        Assert::stringNotEmpty($base);
+        Assert::stringNotEmpty($head);
+
+        return $this->github->repo()->merge(
+            $repository->vendor(),
+            $repository->name(),
+            $base,
+            $head
+        );
+    }
+}
