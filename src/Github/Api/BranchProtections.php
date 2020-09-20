025107173eb04939fb7202a43f82e5368da57b17
Oskar Stark <oskarstark@googlemail.com>
2020-09-20 14:21:13 +0200

    Enhancement: Add and use dedicated BranchesProtections API

---
+++ src/Github/Api/BranchProtections.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Github\Api;
+
+use App\Domain\Value\Branch;
+use App\Domain\Value\Repository;
+use Github\Client as GithubClient;
+
+/**
+ * @author Oskar Stark <oskarstark@googlemail.com>
+ */
+final class BranchProtections
+{
+    private GithubClient $github;
+
+    public function __construct(GithubClient $github)
+    {
+        $this->github = $github;
+    }
+
+    public function update(Repository $repository, Branch $branch, array $params): void
+    {
+        $this->github->repo()->protection()->update(
+            $repository->vendor(),
+            $repository->name(),
+            $branch->name(),
+            $params
+        );
+    }
+}
