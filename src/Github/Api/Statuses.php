68e2eb435bca4d6abfa8f10119309f731bc66511
Oskar Stark <oskarstark@googlemail.com>
2020-09-21 14:02:50 +0200

    Enhancement: Introduce and use dedicated Statuses API

---
+++ src/Github/Api/Statuses.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Github\Api;
+
+use App\Domain\Value\Repository;
+use App\Github\Domain\Value\PullRequest;
+use Github\Client as GithubClient;
+
+/**
+ * @author Oskar Stark <oskarstark@googlemail.com>
+ */
+final class Statuses
+{
+    private GithubClient $github;
+
+    public function __construct(GithubClient $github)
+    {
+        $this->github = $github;
+    }
+
+    public function combined(Repository $repository, PullRequest $pullRequest): PullRequest\CombinedStatus
+    {
+        $response = $this->github->repos()->statuses()->combined(
+            $repository->vendor(),
+            $repository->name(),
+            $pullRequest->head()->sha()
+        );
+
+        return PullRequest\CombinedStatus::fromResponse($response);
+    }
+}
