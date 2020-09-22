1612c475517f3a28485640c8745bb94f9d4e4b5c
Oskar Stark <oskarstark@googlemail.com>
2020-09-22 22:42:29 +0200

    Fix: Require parameter

--- src/Github/Domain/Value/PullRequest.php
+++ src/Github/Domain/Value/PullRequest.php
@@ -31,7 +31,7 @@ final class PullRequest
     private User $user;
     private ?bool $mergeable;
 
-    private function __construct(Issue $issue, string $title, string $updatedAt, Base $base, Head $head, User $user, ?bool $mergeable = null)
+    private function __construct(Issue $issue, string $title, string $updatedAt, Base $base, Head $head, User $user, ?bool $mergeable)
     {
         Assert::stringNotEmpty($title);
         Assert::stringNotEmpty($updatedAt);
