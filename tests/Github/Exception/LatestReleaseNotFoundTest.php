4f2ae2e544eff06b4812f5c7d500d448a6ca1719
Oskar Stark <oskarstark@googlemail.com>
2020-09-25 15:08:51 +0200

    Enhancement: Add dedicated exceptions

---
+++ tests/Github/Exception/LatestReleaseNotFoundTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Github\Exception;
+
+use App\Domain\Value\Repository;
+use App\Github\Exception\LatestReleaseNotFound;
+use App\Tests\Util\Helper;
+use Packagist\Api\Result\Package;
+use PHPUnit\Framework\TestCase;
+
+final class LatestReleaseNotFoundTest extends TestCase
+{
+    use Helper;
+
+    /**
+     * @test
+     */
+    public function forRepository()
+    {
+        $package = new Package();
+        $package->fromArray([
+            'name' => $packageName = 'sonata-project/admin-bundle',
+            'repository' => 'https://github.com/sonata-project/SonataAdminBundle',
+        ]);
+
+        $repository = Repository::fromPackage($package);
+
+        $latestReleaseNotFound = LatestReleaseNotFound::forRepository($repository);
+
+        self::assertInstanceOf(
+            \RuntimeException::class,
+            $latestReleaseNotFound
+        );
+        self::assertSame(
+            sprintf(
+                'Could not find latest Release for "%s".',
+                $repository->toString()
+            ),
+            $latestReleaseNotFound->getMessage()
+        );
+    }
+}
