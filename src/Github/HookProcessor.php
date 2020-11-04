9f9d4d2526ae824493f2cc8035c04965fb74a66e
Oskar Stark <oskarstark@googlemail.com>
2020-11-04 15:53:04 +0100

    Magic action "/request-release" (#1240)

--- src/Github/HookProcessor.php
+++ src/Github/HookProcessor.php
@@ -22,6 +22,8 @@ use App\Github\Domain\Value\IncomingWebhook\Event;
 use App\Github\Domain\Value\IncomingWebhook\Payload;
 use App\Github\Domain\Value\Label;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use function Symfony\Component\String\u;
 use Webmozart\Assert\Assert;
 
@@ -33,12 +35,14 @@ final class HookProcessor
 {
     private Issues $issues;
     private Comments $comments;
+    private NotifierInterface $notifier;
     private LoggerInterface $logger;
 
-    public function __construct(Issues $issues, Comments $comments, LoggerInterface $logger)
+    public function __construct(Issues $issues, Comments $comments, NotifierInterface $notifier, LoggerInterface $logger)
     {
         $this->issues = $issues;
         $this->comments = $comments;
+        $this->notifier = $notifier;
         $this->logger = $logger;
     }
 
@@ -86,6 +90,106 @@ final class HookProcessor
     }
 
     /**
+     * Performs an action and reports back via comment:.
+     *
+     * 1) "/request-release" - requests a release of a bundle in Slack's #releases channel
+     */
+    public function magicAction(Payload $payload): void
+    {
+        $this->logger->debug(
+            'Handle incoming webhook',
+            [
+                'method' => __METHOD__,
+            ]
+        );
+
+        $magicActions = [
+            '/request-release' => 'request-release.markdown',
+        ];
+
+        if (!$payload->action()->equals(Action::CREATED())) {
+            $this->logger->debug('Action is not "created", skipping!');
+
+            return;
+        }
+
+        if (!$payload->event()->equalsOneOf([Event::ISSUE(), Event::ISSUE_COMMENT()])) {
+            $this->logger->debug('Event is not "issue" or "issue_comment", skipping!');
+
+            return;
+        }
+
+        if (!$payload->hasComment()) {
+            return;
+        }
+
+        $comment = $payload->comment();
+        Assert::isInstanceOf($comment, Comment::class);
+
+        foreach ($magicActions as $body => $filename) {
+            if ($body !== $comment->body()) {
+                $this->logger->debug(sprintf(
+                    'Comment is not "%s", skipping!',
+                    $body
+                ));
+
+                continue;
+            }
+
+            $repository = Repository::fromIncomingWebhookPayload($payload);
+
+            $filepath = sprintf(
+                '%s/../../templates/github/%s',
+                __DIR__,
+                $filename
+            );
+
+            if (!file_exists($filepath)) {
+                $this->logger->debug(sprintf(
+                    'Could not find file "%s" for "%s", skipping!',
+                    $filepath,
+                    $body
+                ));
+
+                continue;
+            }
+
+            $fileContent = file_get_contents($filepath);
+            Assert::string($fileContent);
+
+            $contents = u($fileContent)
+                ->replace('#handle#', $comment->author()->handle())
+                ->replace('#project#', $payload->repository()->name())
+                ->toString();
+
+            $this->comments->create(
+                $repository,
+                $payload->issue(),
+                $contents
+            );
+
+            $this->comments->remove(
+                $repository,
+                $comment
+            );
+
+            if ('/request-release' === $body) {
+                $notification = new Notification(sprintf(
+                    '%s requested a release for %s in %s',
+                    $comment->author()->handle(),
+                    $payload->repository()->name(),
+                    $payload->url()
+                ));
+                $notification->channels([
+                    'chat/slack',
+                ]);
+
+                $this->notifier->send($notification);
+            }
+        }
+    }
+
+    /**
      * Adds a comment when a comment with a dedicated content ist created + it removes the comment with the dedicated content:.
      *
      * 1) "/help" - shows all supported commands
@@ -126,7 +230,7 @@ final class HookProcessor
         Assert::isInstanceOf($comment, Comment::class);
 
         foreach ($magicComments as $body => $filename) {
-            if ($body !== trim($comment->body())) {
+            if ($body !== $comment->body()) {
                 $this->logger->debug(sprintf(
                     'Comment is not "%s", skipping!',
                     $body
