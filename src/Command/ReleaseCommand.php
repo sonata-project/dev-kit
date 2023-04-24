95323e47798f0a15eb1f8549aea997f0660e8af6
Jordi Sala Morales <jordism91@gmail.com>
2023-04-24 23:17:01 +0200

    Create PR automatically (#2421)

--- src/Command/ReleaseCommand.php
+++ src/Command/ReleaseCommand.php
@@ -17,20 +17,30 @@ use App\Action\DetermineNextRelease;
 use App\Action\Exception\NoPullRequestsMergedSinceLastRelease;
 use App\Config\Projects;
 use App\Domain\Value\Branch;
+use App\Domain\Value\NextRelease;
 use App\Domain\Value\Project;
 use App\Domain\Value\Stability;
+use App\Git\GitManipulator;
+use App\Github\Api\PullRequests;
+use App\Github\Api\Releases;
 use App\Github\Domain\Value\CheckRuns;
 use App\Github\Domain\Value\CombinedStatus;
 use App\Github\Domain\Value\Label;
 use App\Github\Domain\Value\PullRequest;
+use Gitonomy\Git\Repository;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Question\Question;
 use Webmozart\Assert\Assert;
+
+use function Symfony\Component\String\u;
 
 /**
  * @author Jordi Sala <jordism91@gmail.com>
@@ -39,15 +49,24 @@ final class ReleaseCommand extends AbstractCommand
 {
     private Projects $projects;
     private DetermineNextRelease $determineNextRelease;
+    private GitManipulator $gitManipulator;
+    private PullRequests $pullRequests;
+    private Releases $releases;
 
     public function __construct(
         Projects $projects,
-        DetermineNextRelease $determineNextRelease
+        DetermineNextRelease $determineNextRelease,
+        GitManipulator $gitManipulator,
+        PullRequests $pullRequests,
+        Releases $releases
     ) {
         parent::__construct();
 
         $this->projects = $projects;
         $this->determineNextRelease = $determineNextRelease;
+        $this->gitManipulator = $gitManipulator;
+        $this->pullRequests = $pullRequests;
+        $this->releases = $releases;
     }
 
     protected function configure(): void
@@ -82,7 +101,8 @@ final class ReleaseCommand extends AbstractCommand
             ->setDescription('Helps with a project release.')
             ->setHelp($help)
             ->addArgument('project', InputArgument::OPTIONAL, 'Name of the project to release', null)
-            ->addArgument('branch', InputArgument::OPTIONAL, 'Branch of the project to release', null);
+            ->addArgument('branch', InputArgument::OPTIONAL, 'Branch of the project to release', null)
+            ->addOption('pr', null, InputOption::VALUE_NONE, 'Create the release PR automatically');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -92,7 +112,13 @@ final class ReleaseCommand extends AbstractCommand
 
         $this->io->getErrorStyle()->title($project->name());
 
-        return $this->renderNextRelease($project, $branch);
+        $nextRelease = $this->renderNextRelease($project, $branch);
+
+        if (null !== $nextRelease && $this->createPR($input, $output)) {
+            $this->prepareReleasePR($nextRelease);
+        }
+
+        return Command::SUCCESS;
     }
 
     private function selectProject(InputInterface $input, OutputInterface $output): Project
@@ -144,7 +170,22 @@ final class ReleaseCommand extends AbstractCommand
         return $branch;
     }
 
-    private function renderNextRelease(Project $project, Branch $branch): int
+    private function createPR(InputInterface $input, OutputInterface $output): bool
+    {
+        if ($input->getOption('pr')) {
+            return true;
+        }
+
+        $question = new ConfirmationQuestion('Do you want to create a PR to release? (y/N) ', false);
+
+        $helper = $this->getQuestionHelper();
+        $doReleasePR = $helper->ask($input, $output, $question);
+        Assert::boolean($doReleasePR);
+
+        return $doReleasePR;
+    }
+
+    private function renderNextRelease(Project $project, Branch $branch): ?NextRelease
     {
         $notificationStyle = $this->io->getErrorStyle();
         try {
@@ -152,13 +193,13 @@ final class ReleaseCommand extends AbstractCommand
         } catch (NoPullRequestsMergedSinceLastRelease $e) {
             $notificationStyle->warning($e->getMessage());
 
-            return 0;
+            return null;
         }
 
         if (!$nextRelease->isNeeded()) {
             $notificationStyle->warning('Release is not needed');
 
-            return 0;
+            return null;
         }
 
         $this->renderCombinedStatus($nextRelease->combinedStatus());
@@ -179,7 +220,7 @@ final class ReleaseCommand extends AbstractCommand
             ));
             $notificationStyle->warning('Please check labels and changelogs of the pull requests!');
 
-            return 1;
+            throw new \RuntimeException('Fix labels and changelogs on merged pull requests.');
         }
 
         $notificationStyle->success(sprintf(
@@ -192,7 +233,7 @@ final class ReleaseCommand extends AbstractCommand
         // Send markdown to stdout and only that
         $this->io->writeln($nextRelease->changelog()->asMarkdown());
 
-        return 0;
+        return $nextRelease;
     }
 
     private function renderPullRequest(PullRequest $pr): void
@@ -297,6 +338,74 @@ final class ReleaseCommand extends AbstractCommand
         }
 
         $table->render();
+    }
+
+    private function prepareReleasePR(NextRelease $nextRelease): void
+    {
+        $notificationStyle = $this->io->getErrorStyle();
+        $notificationStyle->section('Create PR to release');
+
+        $gitRepository = $this->gitManipulator->gitCloneProject($nextRelease->project());
+
+        $devKitBranchName = $this->gitManipulator->prepareBranch($gitRepository, $nextRelease->branch(), '-release-'.$nextRelease->nextTag()->toString());
+
+        $this->updateChangelog($gitRepository, $nextRelease);
+
+        $gitRepository->run('add', ['.', '--all']);
+        $diff = $gitRepository->run('diff', ['--color', '--cached', sprintf('origin/%s', $nextRelease->branch()->name())]);
+
+        if ('' !== $diff) {
+            $this->io->writeln($diff);
+
+            $changes = $gitRepository->run('status', ['-s']);
+
+            if ('' !== $changes) {
+                $gitRepository->run('commit', ['-m', $nextRelease->nextTag()->toString()]);
+                $gitRepository->run('push', ['-u', 'origin', $devKitBranchName]);
+            }
+
+            $currentHead = u('sonata-project:')->append($devKitBranchName)->toString();
+
+            // If the Pull Request does not exists yet, create it.
+            if (!$this->pullRequests->hasOpenPullRequest($nextRelease->project()->repository(), $currentHead)) {
+                $this->pullRequests->create(
+                    $nextRelease->project()->repository(),
+                    sprintf(
+                        'Release %s',
+                        $nextRelease->nextTag()->toString()
+                    ),
+                    $currentHead,
+                    $nextRelease->branch()->name(),
+                    "This PR was created automatically by the `sonata-project/dev-kit` project.\nMake sure to manually replace the `@deprecated` comments with the appropriate version before merging."
+                );
+                $this->releases->createDraft($nextRelease);
+            }
+        }
+    }
+
+    private function updateChangelog(Repository $gitRepository, NextRelease $nextRelease): void
+    {
+        $changelogFilePath = $gitRepository->getPath().'/CHANGELOG.md';
+
+        $changelogFileContents = file_get_contents($changelogFilePath);
+
+        if (false === $changelogFileContents) {
+            throw new \RuntimeException(sprintf(
+                'Cannot read "%s" file',
+                $changelogFilePath
+            ));
+        }
+
+        $replaced = preg_replace('/(This project adheres to \[Semantic Versioning\]\(http:\/\/semver.org\/\)\.)\n/', '$1'.\PHP_EOL.\PHP_EOL.$nextRelease->changelog()->asMarkdown(), $changelogFileContents);
+
+        $res = file_put_contents($changelogFilePath, $replaced);
+
+        if (false === $res) {
+            throw new \RuntimeException(sprintf(
+                'Cannot write "%s" file',
+                $changelogFilePath
+            ));
+        }
     }
 
     /**
