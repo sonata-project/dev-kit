a8c5fa576355bbdf43348a96189dcfef540a5875
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-12-05 17:01:49 +0100

    Fix

--- rector.php
+++ rector.php
@@ -13,8 +13,8 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php70\Rector\FunctionLike\ExceptionHandlerTypehintRector;
-use Rector\Php71\Rector\FuncCall\CountOnNullRector;
-use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
@@ -34,8 +34,8 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->importNames();
     $rectorConfig->importShortClasses(false);
     $rectorConfig->skip([
-        CountOnNullRector::class,
         ExceptionHandlerTypehintRector::class,
         AddSeeTestAnnotationRector::class,
+        PreferPHPUnitThisCallRector::class,
     ]);
 };
